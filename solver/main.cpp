16114dc089190b92db7b475c8271db3d353a0a04
Apoorva Appadoo Srinivas <apoorvaappadoo@gmail.com>
2024-02-16 14:30:33 +0100

    remove bullshit

--- solver/main.cpp
+++ solver/main.cpp
@@ -7,13 +7,8 @@
 #include "piece_loader/piece_loader.h"
 
 void thread_function(int board_size, std::vector<PIECE> pieces, Board &max_board, int &max_count, std::mutex &mutex) {
-
     Board board = create_board(board_size);
-    auto start = std::chrono::high_resolution_clock::now();
     solve_board(board, pieces, max_board, max_count, mutex);
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsed = end - start;
-    std::cout << board_size << "x" << board_size << " board solved in " << elapsed.count() << " seconds" << std::endl;
     std::vector<std::string> board_lines = board_to_string(board);
     for (auto const &line: board_lines) {
         std::cout << line << std::endl;
@@ -39,6 +34,7 @@ int main(int argc, char *argv[]) {
     int max_count = 0;
     std::vector<std::thread> threads;
     threads.reserve(16);
+    auto start = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < 16; i++) {
         threads.emplace_back(thread_function,
                              board_size,
@@ -49,8 +45,7 @@ int main(int argc, char *argv[]) {
                              );
     }
     // every 2 seconds, print the current max count
-    bool stop = false;
-    while (!stop) {
+    while (true) {
         std::this_thread::sleep_for(std::chrono::seconds(2));
         std::cout << "Max count: " << max_count << std::endl;
         std::vector<std::string> board_lines = board_to_string(max_board);
@@ -59,6 +54,14 @@ int main(int argc, char *argv[]) {
         }
         // if board is solved, stop the threads
         if (max_count == board_size * board_size) {
+            auto end = std::chrono::high_resolution_clock::now();
+           // stop threads
+            for (auto &thread: threads) {
+                thread.join();
+            }
+            // export the board
+            std::chrono::duration<double> elapsed = end - start;
+            std::cout << board_size << "x" << board_size << " board solved in " << elapsed.count() << " seconds" << std::endl;
             export_board(max_board);
             return 0;
         }
