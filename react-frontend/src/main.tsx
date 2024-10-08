e996fb3fc9704649579abbec0a95884d335a1c57
Ozeliurs <ozeliurs@gmail.com>
2024-10-08 15:05:56 +0200

    refactor(main.tsx): remove unused imports

--- react-frontend/src/main.tsx
+++ react-frontend/src/main.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { CssBaseline, Paper, ThemeProvider } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import theme from "./theme";
-import Test from "./Test.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { RootLayout } from "./RootLayout";
-import { HomePage } from "./pages/homepage/HomePage";
 import { RecoilRoot } from "recoil";
 import { Solver } from "./pages/solver/Solver.tsx";
 import PathManager from "./pages/pathManager/PathManager.tsx";
-import { RequestFormStatistics } from "./pages/statistics/RequestFormStatistics.tsx";
 import { StatisticsSolver } from "./pages/statistics/StatisticsSolver.tsx";
 
 const router = createBrowserRouter([
