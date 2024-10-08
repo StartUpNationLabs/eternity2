bd227aeb879054842436d6dfe9706a1447962f25
Ozeliurs <ozeliurs@gmail.com>
2024-10-08 17:26:28 +0200

    fix(ResponsiveAppBar): update anchor href from "#" to "/" for better navigation

--- react-frontend/src/components/ResponsiveAppBar.tsx
+++ react-frontend/src/components/ResponsiveAppBar.tsx
@@ -60,7 +60,7 @@ function ResponsiveAppBar() {
             variant="h6"
             noWrap
             component="a"
-            href="#"
+            href="/"
             sx={{
               mr: 2,
               display: { xs: "none", md: "flex" },
@@ -115,7 +115,7 @@ function ResponsiveAppBar() {
             variant="h5"
             noWrap
             component="a"
-            href="#"
+            href="/"
             sx={{
               mr: 2,
               display: { xs: "flex", md: "none" },
