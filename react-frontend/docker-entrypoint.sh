1e80ce933602c6f10bf12b163c0ab885542f55ae
Ozeliurs <ozeliurs@gmail.com>
2024-10-08 16:52:37 +0200

    refactor(docker-entrypoint.sh): improve env variable substitution by adding recursive directory processing

--- react-frontend/docker-entrypoint.sh
+++ react-frontend/docker-entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/env bash
 
-for file in /usr/share/nginx/html/*; do
-    if [ -f "$file" ]; then
-        envsubst < "$file" > "$file.tmp" && mv "$file.tmp" "$file"
-    fi
-done
+process_directory() {
+    for item in "$1"/*; do
+        if [ -f "$item" ]; then
+            envsubst < "$item" > "$item.tmp" && mv "$item.tmp" "$item"
+        elif [ -d "$item" ]; then
+            process_directory "$item"
+        fi
+    done
+}
+
+process_directory "/usr/share/nginx/html"
 
 nginx -g "daemon off;"
